a29c3ae46dd3ea4e1dfcb0f7f7a651f9c3e1b682
KB-0907 <106339182+KB-0907@users.noreply.github.com>
2022-06-25 02:57:58 +0900

    update

--- app/src/main/java/com/example/kdaproject001/MainActivity.java
+++ app/src/main/java/com/example/kdaproject001/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.kdaproject001;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.app.ApplicationErrorReport;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Button schButton;
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         schButton = findViewById(R.id.schedule);
+        pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(2);
+
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
+
+        ScheduleFragment schFragment = new ScheduleFragment();
+        adapter.addItem(schFragment);
+
+        CreditFragment creFragment = new CreditFragment();
+        adapter.addItem(creFragment);
+
+        pager.setAdapter(adapter);
 
         schButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,4 +46,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    class MyPagerAdapter extends FragmentStatePagerAdapter {
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+        public MyPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+    }
+
 }
