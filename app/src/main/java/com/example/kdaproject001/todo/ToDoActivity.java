e6b8e977d5478ee568e85e4581a0668c0ea4f4fd
arachne <1022lsh@naver.com>
2022-08-02 22:50:11 +0900

    0101010

--- app/src/main/java/com/example/kdaproject001/todo/ToDoActivity.java
+++ app/src/main/java/com/example/kdaproject001/todo/ToDoActivity.java
@@ -1,5 +1,5 @@
 package com.example.kdaproject001.todo;
-
+// 도레미파솔라시도
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,16 +9,27 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
+import com.example.kdaproject001.MainActivity;
 import com.example.kdaproject001.R;
 import com.example.kdaproject001.board.FreeBoardListActivity;
+import com.example.kdaproject001.board.ModifyPostActivity;
 import com.example.kdaproject001.board.PostAdapter;
 import com.example.kdaproject001.board.PostInfo;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -45,9 +56,10 @@ public class ToDoActivity extends AppCompatActivity {
     RecyclerView assignRCV, examRCV;
     AssignAdapter AsAdapter;
     ExamAdapter examAdapter;
-
+    ImageButton beforeBtn;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); //파이어 베이스에서 현재 로그인한 유저
+    String currentUserID = user.getUid(); //현재 로그인한 사용자 uid
     int y = 0, m = 0, d = 0, h = 0, mi = 0;
-
     Map<String, Object> data = new HashMap<>();
     ArrayList<String> dataList;
 
@@ -60,6 +72,15 @@ public class ToDoActivity extends AppCompatActivity {
 
         generateAssignRCV();
         generateExamRCV();
+
+        beforeBtn = findViewById(R.id.before_btn);
+        beforeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
     }
 
@@ -115,7 +136,9 @@ public class ToDoActivity extends AppCompatActivity {
                             dataList = new ArrayList<>();
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d(TAG, document.getId() + " => " + document.getData());
-                                if (document.getData().get("sort").toString().equals("assign"))
+                                Log.d("파이어베이스 아이디", currentUserID);
+                                Log.d("현재 아이디", document.getData().get("UID").toString());
+                                if (document.getData().get("sort").toString().equals("assign") && currentUserID.equals(document.getData().get("UID").toString()))
                                     dataList.add(document.getData().get("title").toString());
                             }
 
